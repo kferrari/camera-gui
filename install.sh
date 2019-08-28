2fe2cf997bc955ac9c2e421574c3cd9b554f9d57
Kim Ferrari <kim.ferrari@uzh.ch>
2019-08-28 13:39:57 +0200

    try some tests

--- install.sh
+++ install.sh
@@ -10,15 +10,25 @@ sudo apt-get update
 sudo apt-get install raspi-config
 
 # Create Code folder and cd into it
+echo "Creating Code folder"
 sudo mkdir -p /home/pi/Code
 cd /home/pi/Code
 sudo chown -R travis /home/pi/Code
 
-# Clone into pupillometry repository
+# Clone into camera-gui repository
+echo "Cloning camera-gui repository"
 git clone https://github.com/kferrari/camera-gui.git # change this to public repo
+if [ ! -d "camera-gui" ]; then
+  exit 1
+fi
 
 # Enable camera
-sudo raspi-config nonint do_camera 0
+echo "Enabling camera..."
+if sudo raspi-config nonint do_camera 0 ; then
+  echo "Success"
+else
+  exit $?
+fi
 
 # Enable I2C for BrightPi control
 sudo raspi-config nonint do_i2c 0
@@ -30,6 +40,8 @@ sudo apt-get install python-smbus -y
 git clone https://github.com/PiSupply/Bright-Pi.git
 cd Bright-Pi
 sudo python setup.py install
+
+exit $?
 
 # Reboot raspberry
 whiptail --msgbox "The system will now reboot" 8 40
